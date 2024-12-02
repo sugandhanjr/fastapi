057f9eba13a99c45f7fd0f396c81800f38ed53e0
sugandhanjr <sudandhanjr@gmail.com>
2024-12-02 17:46:16 +0530

    2.10

---
+++ main.py
@@ -0,0 +1,73 @@
+from typing import Optional
+from fastapi import FastAPI,Response,status,HTTPException
+from fastapi.params import Body
+from pydantic import BaseModel
+from random import randrange
+
+app = FastAPI()
+
+# Data model for a post
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+# In-memory data store
+my_posts = [
+    {"title": "Title of Post 1", "content": "Content of Post 1", "id": 1},
+    {"title": "Favorite Foods", "content": "I like pizza", "id": 2}]
+
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
+        
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
+
+
+# Root endpoint
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
+
+# Get all posts
+@app.get("/posts")
+def get_posts():
+    return {"data": my_posts}
+
+# Create a new post
+@app.post("/posts",status_code=status.HTTP_201_CREATED)
+def create_post(post: Post):
+    post_dict = post.dict()
+    post_dict["id"] = randrange(0, 1000000)  # Assign a unique random ID
+    my_posts.append(post_dict)
+    return {"data": post_dict}
+
+  
+
+# Get a specific post by ID
+@app.get("/posts/{id}")
+def get_post(id: int ,responce :Response):
+
+    post = find_post(id)
+    if not post:
+      raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"post with id:{id} was not found")
+
+    return{"post_detail":post}
+ 
+
+@app.delete("/posts/{id}",status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    # deleting post
+    #find the index in the array that has required id 
+    #my_posts.pop(index)
+     index = find_index_post(id)
+
+     if index == None:
+         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"post with id:{id} dose not exist")
+     my_posts.pop(index)
+     return Response(status_code=status.HTTP_204_NO_CONTENT)
